e841d4afcdc61ff59fe94c00fe29b6adb9aa8906
Rohit Patil <rohts.patil@gmail.com>
2017-06-04 03:00:31 +0530

    populated the functions

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,27 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+
+  Kp = Kp;
+	Ki = Ki;
+	Kd = Kd;
+
+	p_error = 0.0;
+	i_error = 0.0;
+	d_error = 0.0;
+
 }
 
 void PID::UpdateError(double cte) {
+
+  d_error = cte - prev_cte;
+  i_error = i_error + cte;
+  p_error = cte;
+
 }
 
 double PID::TotalError() {
-}
 
+ return -Kp * p_error - Ki * i_error - Kd * d_error;
+
+}
